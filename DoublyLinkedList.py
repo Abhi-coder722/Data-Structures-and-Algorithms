ec8deb91470908f7ae24be35c9590cc53df079c8
Abhi-coder722 <109688594+Abhi-coder722@users.noreply.github.com>
2022-12-24 11:23:00 +0100

    Update DoublyLinkedList.py

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,8 +1,11 @@
 #doubly linked list........
 
 #It is a collection od nodes containing the three parts
-#namely data part,prev address and the next pasrt.....
+#namely data part, previous node's address and the next node's address.....
 
+
+
+#creating a node
 class Node:
     def __init__(self,data):
         self.data=data
@@ -13,9 +16,9 @@ class DLL:
         self.head=None
         self.tail=None
         
+    #adding element at the beginning of the doubly linked list
     def addbeg(self,data):
         new_node=Node(data)
-
         if self.head is None:
             self.head=new_node
             self.tail=new_node
@@ -24,7 +27,8 @@ class DLL:
             new_node.next=self.head
             self.head.prev=new_node
             self.head=new_node
-
+            
+    #traversing the list i.e. reading elements from the list
     def traverse(self):
         if self.head is None:
             print("The list is empty..")
@@ -33,6 +37,8 @@ class DLL:
             while n is not None:
                 print(str(n.data)+"  ",end="")
                 n=n.next
+    
+    #adding element at the end 
     def addend(self,data):
         new_node=Node(data)
         if self.head is None:
@@ -43,6 +49,8 @@ class DLL:
             self.tail.next=new_node
             new_node.prev=self.tail
             self.tail=new_node
+            
+    #Deleting the first element from the list
     def delbeg(self):
         if self.head is None:
             print("The list is empty...")
@@ -52,6 +60,8 @@ class DLL:
             self.head=t.next
             self.head.prev=None
             del(t)
+            
+    #Deleting the last element from the list
     def delend(self):
         if self.head is None:
             print("The list is empty...")
@@ -60,6 +70,8 @@ class DLL:
             t=self.tail
             t.prev.next=None
             del(t)
+            
+    #inserting the element at a specified position in the list
     def inspos(self,data,posi):
         new_node=Node(data)
         n=self.head
@@ -72,6 +84,7 @@ class DLL:
         n.next.prev=new_node
         n.next=new_node
         
+    #deleting the element from a specified position in the list
     def delpos(self,pos):
         if self.head is None:
             print("The list is empty...")
@@ -84,6 +97,10 @@ class DLL:
                 n=n.next
             n.next=n.next.next
             n.next.prev=n
+            
+            
+            
+ # all the above operations can be performed 
 c=DLL()
 c.addbeg(38)
 c.addbeg(45)
